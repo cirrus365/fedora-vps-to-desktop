e6e66ffcccf976181ab65b58c78b8302130fd3fb
cirrus365 <217430270+cirrus365@users.noreply.github.com>
2026-05-16 20:12:43 +1200

    set correct url

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-REPO_URL="https://github.com/markeast/fedora-vps-to-desktop"
+REPO_URL="https://github.com/cirrus365/fedora-vps-to-desktop"
 INSTALL_DIR="/opt/vdp"
 
 # Silent mode: VDP_USERNAME, VDP_USER_PASSWORD, VDP_VNC_PASSWORD, VDP_DOMAIN, VDP_TIMEZONE
